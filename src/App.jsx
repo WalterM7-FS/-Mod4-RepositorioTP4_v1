466c0f9ab7c79ef5c5e6215f347effd4d3be3e12
Walter Alejandro Marchioli <waltermarchioli@gmail.com>
2025-04-12 14:48:54 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header"
 import Cart from "./components/Cart"
 import ProductList from "./components/ProductList"
 import ThemeToggleButton from './components/ThemeToggleButton'
-import Data from './components/data'
+import Data from './components/Data'
 
 function App() {
   const [characters, setCharacters] = useState([]) 
